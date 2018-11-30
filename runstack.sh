91a13d037ba76dc44303f2ba4e5f99a3177c12a4
Marco A. Mercado Lugo <marco.mercado@g-tec.io>
2018-11-30 12:33:07 -0600

    CloudFormation deployment files added

---
+++ runstack.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+aws cloudformation $ACTION \
+    --region us-east-1 \
+    --stack-name $STACK_NAME \
+    --template-body file://service.yaml \
+    --capabilities CAPABILITY_NAMED_IAM \
+    --parameters \
+    ParameterKey=CreatorImage,ParameterValue=805495149875.dkr.ecr.us-east-1.amazonaws.com/cms-creator:$(git rev-parse HEAD) \
+    ParameterKey=EditorImage,ParameterValue=805495149875.dkr.ecr.us-east-1.amazonaws.com/cms-editor$(git rev-parse HEAD) \
+    ParameterKey=ViewerImage,ParameterValue=805495149875.dkr.ecr.us-east-1.amazonaws.com/cms-viewer:$(git rev-parse HEAD) \
+    ParameterKey=VPC,ParameterValue=vpc-01795934d771ba8c8 \
+    ParameterKey=Cluster,ParameterValue=cms-websites \
+    ParameterKey=Listener,ParameterValue=arn:aws:elasticloadbalancing:us-west-1:805495149875:listener/app/CMS-websites/97c189cffd04335c/4900ef26966a4def
