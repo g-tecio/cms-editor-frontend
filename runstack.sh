91cf6c18105152fa10217ec65f11df0e20e068b9
Marco A. Mercado Lugo <marco.mercado@g-tec.io>
2018-11-30 21:35:20 -0600

    unitary service

--- runstack.sh
+++ runstack.sh
@@ -6,9 +6,7 @@ aws cloudformation $ACTION \
     --template-body file://service.yaml \
     --capabilities CAPABILITY_NAMED_IAM \
     --parameters \
-    ParameterKey=CreatorImage,ParameterValue=805495149875.dkr.ecr.us-east-1.amazonaws.com/cms-creator:$(git rev-parse HEAD) \
     ParameterKey=EditorImage,ParameterValue=805495149875.dkr.ecr.us-east-1.amazonaws.com/cms-editor:$(git rev-parse HEAD) \
-    ParameterKey=ViewerImage,ParameterValue=805495149875.dkr.ecr.us-east-1.amazonaws.com/cms-viewer:$(git rev-parse HEAD) \
     ParameterKey=VPC,ParameterValue=vpc-01795934d771ba8c8 \
     ParameterKey=Cluster,ParameterValue=cms-websites \
     ParameterKey=Listener,ParameterValue=arn:aws:elasticloadbalancing:us-west-1:805495149875:listener/app/CMS-websites/97c189cffd04335c/4900ef26966a4def
